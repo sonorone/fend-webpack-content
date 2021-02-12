97db6f4a81e8908349266f2bfdfaccc6d1ddb010
Damian Murawiecki <damianmurawiecki@gmail.com>
2021-02-11 21:33:44 -0500

    babel-load and html-webpack plugin added

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,6 @@
-alert("whatsup")
+import { submitHandle } from "./js/formHandler";
+import { checkForName } from "./js/nameChecker";
+
+console.log(checkForName);
+
+alert("Wassup")
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: './src/client/index.js'
+  entry: './src/client/index.js',
+  module: {
+    rules: [
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html"
+    })
+  ]
 }
