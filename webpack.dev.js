6052e9903f579079e9a5568936df7275dc575dfd
Damian Murawiecki <damianmurawiecki@gmail.com>
2021-02-11 21:58:04 -0500

    added dev and prod webpack files

---
+++ webpack.dev.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map', 
+  entry: './src/client/index.js',
+  module: {
+    rules: [
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html"
+    })
+  ]
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
+  mode: 'production',
   entry: './src/client/index.js',
   module: {
     rules: [
